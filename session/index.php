2c34b2da90b0c42724c1dd3afe76799859567326
Ebinezar <55144246+Ebinezar007@users.noreply.github.com>
2021-01-27 17:42:46 +0530

    Add files via upload

---
+++ session/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+session_start();
+?>
+
+
+<?php
+// Echo session variables that were set on previous page
+echo "Favorite color is " . $_SESSION["favcolor"]="yellow". ".<br>";
+echo "Favorite animal is " . $_SESSION["favanimal"]="saran" . ".";
+?>
+
+</body>
+</html>
