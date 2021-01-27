8fb1dd0d41013e42e8333da6eae738cdc6a4aeb0
Ebinezar <55144246+Ebinezar007@users.noreply.github.com>
2021-01-27 17:43:12 +0530

    Update index.php

--- session/index.php
+++ session/index.php
@@ -9,7 +9,7 @@ session_start();
 <?php
 // Echo session variables that were set on previous page
 echo "Favorite color is " . $_SESSION["favcolor"]="yellow". ".<br>";
-echo "Favorite animal is " . $_SESSION["favanimal"]="saran" . ".";
+echo "Favorite animal is " . $_SESSION["favanimal"]="lion" . ".";
 ?>
 
 </body>
